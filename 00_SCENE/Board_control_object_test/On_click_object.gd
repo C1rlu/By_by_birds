909f824dc4703738dd7fa87ea6b199e8ca07acd0
Cirlu <leprovostulric@gmail.com>
2024-10-03 18:39:18 +0200

    ups

--- 00_SCENE/Board_control_object_test/On_click_object.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-
-signal On_Move(condition : bool)
-signal _move(rayCast_result : Dictionary ,speed : float ,delta : float)
-signal _rotate(direction : float)
-
--- 13_TOOLS_UI/Tool_selector_ui/Selector_input.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-
-func _input(event):
-	
-	if event.is_action_pressed("open_selector"):
-		_global_datas.open_tool_selector.emit(true)		

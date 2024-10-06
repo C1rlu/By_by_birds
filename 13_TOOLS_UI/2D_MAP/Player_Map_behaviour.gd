59cc0b9182e70b55a32fd9227f51310848eefdce
Cirlu <leprovostulric@gmail.com>
2024-10-06 15:57:23 +0200

    update

--- 13_TOOLS_UI/2D_MAP/Player_Map_behaviour.gd
+++ 13_TOOLS_UI/2D_MAP/Player_Map_behaviour.gd
@@ -4,7 +4,7 @@ extends Node
 
 
 func _ready():
-	#_global_datas._open_player_map.connect(_open_map)
+	_global_datas._open_player_map.connect(_open_map)
 	
 	Map_node.visible = false
 	
@@ -15,8 +15,3 @@ func _open_map(condition : bool):
 	Map_node.visible = condition	
 	_global_datas.Player_InMenu = condition
 	
-	if condition:
-		_global_datas._add_back_call.emit(back_call)
-
-func back_call():
-	_global_datas._open_player_map.emit(false)

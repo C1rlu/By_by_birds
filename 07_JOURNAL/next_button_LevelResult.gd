1820e3e3e8e8c36fc1a2c4eee3a97dd670fc878a
Cirlu <leprovostulric@gmail.com>
2024-10-24 23:18:24 +0200

    correction done

--- 07_JOURNAL/next_button_LevelResult.gd
+++ 07_JOURNAL/next_button_LevelResult.gd
@@ -12,5 +12,6 @@ func _press():
 	
 	_global_datas.open_owl_view.emit(true)
 	_global_datas.player_owl_moment = true
+	_global_datas._open_proposition.emit(false)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
 	root.queue_free()
